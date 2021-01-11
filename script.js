13acbefc96d4e15d07d0bbeb4d2b0052a6ba92fa
CoolhanD <abrawa@gmail.com>
2021-01-11 04:55:14 +0200

    resize cactus, click function

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const cactus = document.getElementById("cactus");
 document.addEventListener("keydown", function(event) {
     jump();
 });
+document.addEventListener("click", function(event) {
+    jump();
+});
 
 function jump () {
     if (sonic.classList != "jump") {
@@ -15,12 +18,13 @@ function jump () {
         sonic.classList.remove("jump")
     }, 300)
 }
+sonic.onclick = jump();
 
 let isAlive = setInterval ( function() {
     let sonicTop = parseInt(window.getComputedStyle(sonic).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft < 50 && cactusLeft > 0 && sonicTop >= 165) {
+    if (cactusLeft < 50 && cactusLeft > 0 && sonicTop >= 220) {
         alert("You LOSE, WTF!?")
     }
 }, 10)
